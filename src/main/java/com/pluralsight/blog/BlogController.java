fa795255657d36017d08d7abb2dbde4849a433c8
wiliam lee <wiliam.tae@gmail.com>
2021-12-17 10:41:49 -0300

    initialize postRepository

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,5 +1,6 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,5 +12,11 @@ public class BlogController {
     public String listPosts(ModelMap modelMap) {
         modelMap.put("title", "Blog Post 1");
         return "home";
+    }
+
+    private PostRepository postRepository() {
+    }
+
+    public BlogController(postRepository()) {
     }
 }
