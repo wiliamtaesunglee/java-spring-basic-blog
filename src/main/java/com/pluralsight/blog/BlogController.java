96717213164dcabd42e8777fd6b8f14e0bda5828
wiliam lee <wiliam.tae@gmail.com>
2021-12-18 07:27:17 -0300

    post details route

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -4,6 +4,7 @@ import com.pluralsight.blog.data.PostRepository;
 import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -21,5 +22,19 @@ public class BlogController {
         List<Post> posts = postRepository.getAllPosts();
         modelMap.put("posts", posts);
         return "home";
+    }
+
+    @RequestMapping("/post/{id}")
+    public String postDetails(@PathVariable Long id, ModelMap modelMap) {
+        postRepository.findById(id);
+        List<Post> posts = postRepository.getAllPosts();
+//        posts.forEach(k, v -> {
+//            if(v.id == id) {
+//                return v;
+//            } else {
+//                return null;
+//            }
+//        });
+        return "post-details";
     }
 }
